c09d654dfda5203c8020333f34b975d6fe2bd194
yuliujun <1099887856@qq.com>
2020-09-28 15:08:55 +0800

    项目整体搭建springcloud服务 --第二次提交信息---- --于留俊 2020-09-28

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -8,7 +8,7 @@ public class EurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
-		System.out.println("启动成功！！！！");
+		System.out.println("启动成功！！！！=======");
 	}
 
 }
